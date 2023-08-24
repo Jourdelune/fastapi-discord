81f9e79651ab5c0695993993975a03200f0eca66
Jourdelune <64205064+Jourdelune@users.noreply.github.com>
2023-08-24 17:41:36 +0200

    Update user.py

--- fastapi_discord/models/user.py
+++ fastapi_discord/models/user.py
@@ -8,18 +8,16 @@ class User(BaseModel):
     username: str
     discriminator: str
     avatar: Optional[str]
-    avatar_url: Optional[str]
+    avatar_url: Optional[str] = "https://cdn.discordapp.com/embed/avatars/1.png"
     locale: str
     email: Optional[str]
-    bot: Optional[bool]
     mfa_enabled: bool
     flags: int
     premium_type: Optional[int]
     public_flags: int
-
+    global_name: Optional[str]
+    
     def __init__(self, **data: Any):
         super().__init__(**data)
         if self.avatar:
             self.avatar_url = f"https://cdn.discordapp.com/avatars/{self.id}/{self.avatar}.png"
-        else:
-            self.avatar_url = "https://cdn.discordapp.com/embed/avatars/1.png"
